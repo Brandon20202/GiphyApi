e42258d5f6ce45acb98ed6eb63fa881cb9918670
Brandon20202 <brandon.taylor@austinisd.org>
2020-03-08 18:54:32 -0500

    format

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,7 +12,7 @@ var animalArray = [
 ];
 
 
-for ( var i = 0; i < animalArray.length; i++ ) {
+for (var i = 0; i < animalArray.length; i++) {
 	var animalToAdd = animalArray[i];
 	var animalButton = $("<button>");
 
@@ -25,7 +25,7 @@ for ( var i = 0; i < animalArray.length; i++ ) {
 
 };
 
-$("#submit-button").on("click", function(event) {
+$("#submit-button").on("click", function (event) {
 	event.preventDefault();
 
 	var animalToAdd = $("#animal-input").val().trim();
@@ -41,48 +41,48 @@ $("#submit-button").on("click", function(event) {
 });
 
 
-$(document.body).on("click", ".animal-button", function() {
-    var animal = $(this).attr("data-animal");
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=lTYJWbbedHsJivEp3AMK309oTOQsya4N";
+$(document.body).on("click", ".animal-button", function () {
+	var animal = $(this).attr("data-animal");
+	var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=lTYJWbbedHsJivEp3AMK309oTOQsya4N";
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).done(function(response) {
-      	console.log(response);
+	$.ajax({
+		url: queryURL,
+		method: "GET"
+	}).done(function (response) {
+		console.log(response);
 
-      	var results = response.data;
+		var results = response.data;
 
-      	$("#animal-gifs-div").empty();
+		$("#animal-gifs-div").empty();
 
-      	for ( var i = 0; i < results.length; i++ ) {
-      		var animalDiv = $("<div>");
-      		animalDiv.addClass("animal-div");
-        	var rating = results[i].rating
-        	var p = $("<p>").text("Rating: " + rating);
+		for (var i = 0; i < results.length; i++) {
+			var animalDiv = $("<div>");
+			animalDiv.addClass("animal-div");
+			var rating = results[i].rating
+			var p = $("<p>").text("Rating: " + rating);
 
-	        var animalImage = $("<img>").attr("src", results[i].images.fixed_height_still.url);
-	        animalImage.addClass("gif");
-	        animalImage.attr("data-still", results[i].images.fixed_height_still.url);
-	        animalImage.attr("data-animate", results[i].images.fixed_height.url);
-	        animalImage.attr("data-state", "still");
+			var animalImage = $("<img>").attr("src", results[i].images.fixed_height_still.url);
+			animalImage.addClass("gif");
+			animalImage.attr("data-still", results[i].images.fixed_height_still.url);
+			animalImage.attr("data-animate", results[i].images.fixed_height.url);
+			animalImage.attr("data-state", "still");
 
-	        animalDiv.append(p);
-	        animalDiv.append(animalImage);
-	        $("#animal-gifs-div").prepend(animalDiv);
-      	};
+			animalDiv.append(p);
+			animalDiv.append(animalImage);
+			$("#animal-gifs-div").prepend(animalDiv);
+		};
 
-      	$(".gif").on("click", function() {
-      		var state = $(this).attr("data-state");
+		$(".gif").on("click", function () {
+			var state = $(this).attr("data-state");
 
-		    if (state === "still") {
-		    	$(this).attr("src", $(this).attr("data-animate"));
-		        $(this).attr("data-state", "animate");
-		    }
-		    else {
-		        $(this).attr("src", $(this).attr("data-still"));
-		        $(this).attr("data-state", "still");
-		    }
-      	});
-    });
+			if (state === "still") {
+				$(this).attr("src", $(this).attr("data-animate"));
+				$(this).attr("data-state", "animate");
+			}
+			else {
+				$(this).attr("src", $(this).attr("data-still"));
+				$(this).attr("data-state", "still");
+			}
+		});
+	});
 });
